980832716efb4a95d445091bb58db943d73e2037
miguel.tinembart <miguel.tinembart@gmail.com>
2023-04-20 21:58:59 +0200

    copied over first idea for secret santa algorithm  - initial Person Type for testing  - shuffleFunction for shuffling the data set  - Initialize adjacency graph relation with createPerson Graph  - secretSanta function: -> has a randomizing schuffle which creates a basically a linked list -> -> the list end with the first node creating a circlular graph

---
+++ src/lib/secretSanta.ts
@@ -0,0 +1,50 @@
+import _, { create } from "lodash";
+
+interface Person {
+  id: Number;
+  name: String;
+  mail: String;
+  active: boolean;
+  dont?: number;
+}
+
+// Returns new shuffled array
+export function shufflePeople(personList: Person[]): Person[] {
+  const shuffleArray: Person[] = [];
+
+  _.shuffle(_.range(1, personList.length + 1)).map((number) => {
+    const [person] = personList.filter((persona) => persona.id === number);
+    shuffleArray.push(person);
+  });
+
+  return shuffleArray;
+}
+
+export function createPersonGraph(
+  personList: Person[]
+): Map<Person, Array<Person>> {
+  let obj = new Map<Person, Array<Person>>();
+  personList.map((person, index, array) => {
+    const adjacents = array.filter(
+      (otherPerson) =>
+        otherPerson.id != person.dont && otherPerson.id != person.id
+    );
+    obj.set(person, adjacents);
+  });
+  return obj;
+}
+
+// push node to list if it is not already inside of array, if it
+export function SecretSanta<T>(
+  map: Map<T, Array<T>>,
+  list: Map<T, T> | undefined = new Map<T, T>()
+): Map<T, T> | undefined {
+  const first: T = map.values().next().value;
+  const array: T[] = [];
+  map.forEach((value, key, copy) => {
+    array.push(key);
+    let next = _.sample(_.difference(array, value)) || first;
+    list.set(key, next);
+  });
+  return list;
+}
