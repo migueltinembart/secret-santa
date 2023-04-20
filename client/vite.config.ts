a96513dba6886e9cd52ef4a8a41fe1c150d94599
miguel <miguel.tinembart@gmail.com>
2023-04-20 16:19:12 +0200

    add extra config parameters to vite config:  - build is redirected to dist of server build  - added proxy for testing api calls to express server

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "./../dist",
+  },
+  server: {
+    proxy: {
+      "/api": "http://localhost:5000",
+    },
+  },
+});
