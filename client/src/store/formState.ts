c12280286a04f986de1702ff8778c75766e8678f
miguel <miguel.tinembart@gmail.com>
2023-04-24 22:03:13 +0200

    renamed from formstate to codeLoginStore

--- client/src/store/formState.ts
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-
-const secretSantaCodeStore = create((set) => ({
-  code: "",
-  changeCode: (input: String) => set((state: String) => ({ code: input })),
-}));
-
-export { secretSantaCodeStore };
