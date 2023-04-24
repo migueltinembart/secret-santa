e4aa327070a72f3195b3b3b0323b2d7c0a244001
miguel.tinembart <miguel.tinembart@gmail.com>
2023-04-24 23:15:14 +0200

    feat: added routes for root  - display App component inside of root route  - defined error Page showing error Component

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./routes/App";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./routes/error-page";
+
+// define routes here!!!
+const clientRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={clientRouter} />
   </React.StrictMode>
 );
