d067e638635e64986e46d14700a37bd0ef6dae2b
miguel <miguel.tinembart@gmail.com>
2023-04-24 22:01:26 +0200

    created first test fetching site to test connection between client and server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,36 @@
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 import Box from "./components/Box";
-import { secretSantaCodeStore } from "./store/formState";
+import { useStore } from "./store/codeLoginStore";
+import { fetchFromFormGet } from "./lib/fetchHandlers/fetchFromFormAction";
+
+interface Person {
+  id: Number;
+  firstName: String;
+  lastName: String;
+  email: String;
+}
+interface SecretSantaGroup {
+  id: Number;
+  people: Person[];
+}
+
 function App() {
-  const code = secretSantaCodeStore((state) => state.code);
+  const changeCode = useStore((state) => state.changeCode);
+  const code = useStore((state) => state.code);
+  const [test, setTest] = useState<SecretSantaGroup>({
+    id: 0,
+    people: [],
+  });
   return (
     <>
       <div className="w-screen h-screen flex flex-row justify-center items-center">
         <Box grid rows={2} cols={2}>
           <div className="col-span-2 text-center text-3xl">Secret Santa</div>
           <form
-            action="/index/:id"
+            action="api/data"
             method="get"
             className="flex flex-col justify-start"
+            onSubmit={async (e) => setTest(await fetchFromFormGet(e))}
           >
             <label htmlFor="secret-santa-codebox">
               Paste your secret santa code here...
@@ -22,9 +41,14 @@ function App() {
               id="secret-santa-codebox"
               minLength={8}
               maxLength={8}
+              onChange={(e) => {
+                e.preventDefault();
+                changeCode(e.target.value);
+              }}
             ></input>
             <br />
             <input type="submit" value="Submit" className="hover:bg-red-200" />
+            <div>{test.id != 0 && test.people.map((person) => (<div>{person.firstName}</div>))}</div>
           </form>
           <form
             action="/register"
