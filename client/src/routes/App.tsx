03be98374b42abbb05de0f45d55630c7daad639f
miguel <miguel.tinembart@gmail.com>
2023-04-26 14:40:22 +0200

    chore: simplified app component - separated initial navbar in its own component  - content component with an Outlet for React router routes  - the useEffect is used for themechange as per documentation

--- client/src/routes/App.tsx
+++ client/src/routes/App.tsx
@@ -1,69 +1,20 @@
-import { useState } from "react";
-import Box from "../components/Box";
-import { useStore } from "../store/codeLoginStore";
-import { fetchFromFormGet } from "../lib/fetchHandlers/fetchFromFormAction";
-import type { SecretSantaGroup } from "../types/identification";
+import { useEffect } from "react";
+import { themeChange } from "theme-change";
+import NavBar from "../components/NavBar";
+import { Outlet } from "react-router-dom";
+import Content from "../components/Content";
 
 function App() {
-  const changeCode = useStore((state) => state.changeCode);
-  const code = useStore((state) => state.code);
-  const [secretSantaGroup, setSecretSantaGroup] = useState<SecretSantaGroup>({
-    id: 0,
-    people: [],
-  });
+  useEffect(() => {
+    themeChange(false);
+  }, []);
   return (
     <>
-      <div className="w-screen h-screen flex flex-row justify-center items-center">
-        <Box grid rows={2} cols={2}>
-          <div className="col-span-2 text-center text-3xl">Secret Santa</div>
-          <form
-            action="api/data"
-            method="get"
-            className="flex flex-col justify-start"
-            onSubmit={async (e) =>
-              setSecretSantaGroup(await fetchFromFormGet(e))
-            }
-          >
-            <label htmlFor="secret-santa-codebox">
-              Paste your secret santa code here...
-            </label>
-
-            <input
-              type="text"
-              id="secret-santa-codebox"
-              minLength={8}
-              maxLength={8}
-              onChange={(e) => {
-                e.preventDefault();
-                changeCode(e.target.value);
-              }}
-            ></input>
-            <br />
-            <input type="submit" value="Submit" className="hover:bg-red-200" />
-            <div>
-              {secretSantaGroup.id != 0 &&
-                secretSantaGroup.people.map((person) => (
-                  <div>{person.firstName}</div>
-                ))}
-            </div>
-          </form>
-          <form
-            action="/api/register"
-            method="post"
-            className="flex flex-col gap-1"
-          >
-            <label htmlFor="firstName">First Name: </label>
-            <input type="text" name="First Name" id="firstName" />
-            <br />
-            <label htmlFor="lastName">Last Name: </label>
-            <input type="text" name="Last Name" id="lastName" />
-            <br />
-            <label htmlFor="email">E-Mail: </label>
-            <input type="email" name="E-Mail" id="email" />
-            <br />
-            <input type="submit" value="Submit" />
-          </form>
-        </Box>
+      <div className="p-2 flex flex-col h-screen">
+        <NavBar themes={"bumblebee,dark"} />
+        <Content className="px-6 shadow-md flex-1 rounded-md">
+          <Outlet context={"/"} />
+        </Content>
       </div>
     </>
   );
