94171311ed81169c5d76c47b7c1a52ef34f478d4
miguel.tinembart <miguel.tinembart@gmail.com>
2023-04-30 00:35:50 +0200

    feat: home route first look - added new route handling on backend for better react-router handling - 2 forms added, one for guest login and one for registration
    feat: added fetchHandler for postRequest for forms
    - fetchFromFormPost
      - send post request with formEvent
      - sends body as json
    
    feat: Created HomePage with forms
    - state managed in RegistrationStore
    - uses fetchFromFormPost for fetching to backend
    - divided forms for registration and direct login access for guests
    
    feat: added route for registration
    - use jsonParser for parsing json objects
    - respond with statusCode
    
    fix: added express.static middleware
     - unhandled routes "*" returns index.html so react-router can handle routes

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,12 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "..", "dist")));
+
 app.use("/api", apiRouter);
-
-app.get("*", express.static(path.join(__dirname, "..", "dist")));
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "dist", "index.html"));
+});
 app.listen(port, () => {
   console.log("app listening on http://localhost:" + port);
 });
